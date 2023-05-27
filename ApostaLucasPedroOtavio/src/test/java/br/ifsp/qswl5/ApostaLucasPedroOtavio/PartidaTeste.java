9f00e35d21887eaeb3fde4104fa8a395eb50c110
Lucas Ocampos <lucas.bin.ocampos@gmail.com>
2023-05-26 22:01:52 -0300

    First commit

---
+++ ApostaLucasPedroOtavio/src/test/java/br/ifsp/qswl5/ApostaLucasPedroOtavio/PartidaTeste.java
@@ -0,0 +1,16 @@
+package br.ifsp.qswl5.ApostaLucasPedroOtavio;
+
+import org.junit.Test;
+import org.junit.Before;
+
+import junit.framework.TestCase;
+import model.Partida;
+
+
+public class PartidaTeste extends TestCase {
+	
+	@Test
+	public void teste() {
+		assertTrue(true);
+	}
+}
